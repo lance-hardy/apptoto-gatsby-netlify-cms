f356e69edb85d668c68a22af72885b62fb9b19c4
Lance Hardy <lance@apptoto.com>
2020-04-01 09:16:34 -0700

    test nav change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,6 +63,9 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/">
                 Solutions
               </Link>
+              <Link className="navbar-item" to="/">
+                Integrations
+              </Link>
               <Link className="navbar-item" to="/about">
                 About
               </Link>
