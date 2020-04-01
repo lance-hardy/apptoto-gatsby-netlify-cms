adabb2e45434ba96c7502256b0826958071670d6
Lance Hardy <lance@apptoto.com>
2020-04-01 09:45:51 -0700

    add integrations hook into cms

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,7 +63,7 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/">
                 Solutions
               </Link>
-              <Link className="navbar-item" to="/">
+              <Link className="navbar-item" to="/integrations">
                 Integrations
               </Link>
               <Link className="navbar-item" to="/about">
