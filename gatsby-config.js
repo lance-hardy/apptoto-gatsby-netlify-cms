c1a6690591fd8533a43b1007822af8e15f5d65b9
Lance Hardy <lance@apptoto.com>
2020-04-01 11:20:42 -0700

    change index meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Appointment Reminders, Messaging & Online Scheduling | Apptoto',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Reduce no-shows and save time with Apptoto’s online scheduling and appointment reminder software. Easily send automated text, voice and email reminders today!',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
